3bee72a75288ee979b97fab8e731b699585bf3fe
Aarón Fernández <aaronfdezgarcia@gmail.com>
2017-04-24 11:55:55 +0200

    rebase

--- db/repaso.sql
+++ db/repaso.sql
@@ -96,3 +96,14 @@ create view v_dispositivos as
      from dispositivos d
 left join aulas a on d.aula_id = a.id
 left join ordenadores o on d.ordenador_id = o.id;
+
+drop table if exists usuarios cascade;
+
+create table usuarios (
+  id         bigserial   constraint pk_usuarios primary key,
+  nombre     varchar(20) not null constraint uq_usuario_unico unique,
+  password   char(32)    not null
+);
+
+insert into usuarios (nombre, password)
+    values ('admin', md5('admin'));
